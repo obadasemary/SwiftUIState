caf2ab381f87abd938d6b227c10f9226b2e89218
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-08-20 01:19:42 +0300

    Finlize State and Binding

--- SwiftUIState/ContentView.swift
+++ SwiftUIState/ContentView.swift
@@ -9,13 +9,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isPlaying = false
+    
+    @State private var counterBlack = 0
+    @State private var counterYellow = 0
+    @State private var counterRed = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        VStack {
+            
+            Button(action: {
+                // Switch between the play and stop button
+                self.isPlaying.toggle()
+            }) {
+                Image(systemName: isPlaying ? "stop.circle.fill" : "play.circle.fill")
+                    .font(.system(size: 120))
+                    .foregroundColor(isPlaying ? .red : .green)
+            }
+            
+            Text("\(counterBlack + counterYellow + counterRed)")
+                .font(.system(size: 220, weight: .bold, design: .rounded))
+            
+            HStack {
+                CounterButton(counter: $counterBlack, color: .black)
+                CounterButton(counter: $counterYellow, color: .yellow)
+                CounterButton(counter: $counterRed, color: .red)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CounterButton: View {
+    
+    @Binding var counter: Int
+    
+    var color: Color
+    
+    var body: some View {
+        Button(action: {
+            // increment number
+            self.counter += 1
+        }) {
+            Circle()
+                .frame(width: 120, height: 120)
+                .foregroundColor(color)
+                .overlay(
+                    Text("\(counter)")
+                        .font(.system(size: 70, weight: .bold, design: .rounded))
+                        .foregroundColor(.white)
+            )
+        }
     }
 }
